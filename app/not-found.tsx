2b98f97bc8312c255d3df77bb98cb903295b4170
WinsreWu <jerrywu3128@outlook.com>
2024-01-06 15:17:42 +0800

    /text功能加入

---
+++ app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <div>
+            <p>Not Found</p>
+            <p>Seems nothing here. Maybe wrong route segments?</p>
+        </div>
+    );
+}
