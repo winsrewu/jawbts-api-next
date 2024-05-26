9e8a629ff74aafab2dc253c35abc1aca093f2139
winsrewu <78122384+winsrewu@users.noreply.github.com>
2024-05-26 14:39:21 +0800

    feat: add Access-Control-Allow-Origin/Headers headers

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                source: '/:anything*',
+                headers: [
+                    {
+                        key: 'Access-Control-Allow-Origin',
+                        value: '*',
+                    },
+                    {
+                        key: 'Access-Control-Allow-Headers',
+                        value: '*',
+                    }
+                ]
+            }
+        ]
+    }
+}
 
 module.exports = nextConfig
