10b69764b6708803e1965ef3d1d07b2966fba4eb
WisW <78122384+winsrewu@users.noreply.github.com>
2024-06-29 15:58:51 +0800

    fix: wrong charset

--- components/ResponseUtils.ts
+++ components/ResponseUtils.ts
@@ -2,15 +2,15 @@
 
 export class ResponseUtils {
     static successJson(json: any) {
-        return Response.json({ code: 'Success', data: json });
+        return Response.json({ code: 'Success', data: json }, { headers: {"Content-Type": "application/json; charset=utf-8"} });
     }
     static notFound(json: any) {
         return this.json("Failed", json, 404, "Not Found");
     }
     static json(code: string, json: any, status: number, statusText: string) {
-        const blob = new Blob([JSON.stringify({ code: code, data: json }, null, 2)], {
-            type: "application/json",
-        });
-        return new Response(blob, { status: status, statusText: statusText})
+        const blob = new Blob([JSON.stringify({ code: code, data: json }, null, 2)]);
+        return new Response(blob, { status: status, statusText: statusText,
+                headers: {"Content-Type": "application/json; charset=utf-8"}
+            });
     }
 }
