65b956aed4c735bd72cab71822652cbad85a7a73
WisW <78122384+winsrewu@users.noreply.github.com>
2024-08-14 18:16:57 +0800

    feat: update version info

--- app/version/[stuff]/route.ts
+++ app/version/[stuff]/route.ts
@@ -6,11 +6,11 @@ const stuffs: { [key: string]: any } = {
         // checkUrl: "https://baidu.com",
         branches: {
             "fabric-1.17.x": {
-                latestVersion: "1.0.8",
+                latestVersion: "1.0.9",
                 downloadUrl: "https://modrinth.com/mod/noglerr/versions#all-versions"
             },
             "fabric-1.21": {
-                latestVersion: "1.0.8",
+                latestVersion: "1.0.9",
                 downloadUrl: "https://modrinth.com/mod/noglerr/versions#all-versions"
             }
         }
