849a8fa95d4b0871461d8930baad00f2ca93f605
WisW <78122384+winsrewu@users.noreply.github.com>
2024-09-21 12:10:17 +0800

    feat: rm useless files

--- app/version/route.ts
+++
@@ -1,9 +0,0 @@
-import { ResponseUtils } from "@/components/ResponseUtils";
-
-export const runtime = 'edge';
-
-const availableStuff = ["noglerr"];
-
-export function GET() {
-    return ResponseUtils.successJson({"availableStuff": availableStuff});
-}
