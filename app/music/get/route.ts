bd03848b00ef8bcbf50790d37df4087ecf498421
WisW <78122384+winsrewu@users.noreply.github.com>
2024-09-30 19:13:22 +0800

    fix: try fix music playing issue

--- app/music/get/route.ts
+++ app/music/get/route.ts
@@ -16,6 +16,9 @@ export async function GET(request: Request) {
     const id = searchParams.get('id');
     if (!id) return ResponseUtils.missing("Param: id");
 
+    let header_with_ua = new Headers();
+    header_with_ua.set('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36 Edg/127.0.0.0');
+
     if (id.startsWith("B")) {
         let headers = request.headers;
         headers.set('Referer', 'https://bilibili.com/video/' + id.slice(1));
@@ -35,24 +38,39 @@ export async function GET(request: Request) {
         headers.set('Upgrade-Insecure-Requests', '1');
         headers.set('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36 Edg/127.0.0.0');
 
+        let range = headers.get('Range');
+        if (range) {
+            let rs = range.split("-");
+            let end = Number.parseInt(rs[0]) + 300000;
+            if (rs.length == 2) {
+                end = Number.parseInt(rs[1]);
+            }
+            if (rs[0] == "0") {
+                end = 2145;
+            } else {
+                end = Math.min(end, Number.parseInt(rs[0]) + 300000)
+            }
+            headers.set('Range', `bytes=${rs[0]}-${end}`);
+        }
+
         let audio_url;
         const id_r = id.slice(1).split("_");
         try {
             // 缓存2分钟
-            const response = await fetch('https://www.bilibili.com/video/' + id_r[0] + (id_r[1] ? ("?p=" + id_r[1]) : ""), { next: { revalidate: 120 } });
+            const response = await fetch('https://www.bilibili.com/video/' + id_r[0] + (id_r[1] ? ("?p=" + id_r[1]) : ""), { next: { revalidate: 120 }, headers: header_with_ua });
             const text = await response.text();
             const res = text.match(/<script>window.__playinfo__=(.*?)<\/script>/)
             if (!res || !res[1]) {
                 // 有些地方没有playinfo, 需要动用另一个接口来拿地址
 
                 // 注意, 缓存一天
-                const response_cid = await fetch('https://api.bilibili.com/x/player/pagelist?bvid=' + id_r[0], { next: { revalidate: 86400 } });
+                const response_cid = await fetch('https://api.bilibili.com/x/player/pagelist?bvid=' + id_r[0], { next: { revalidate: 86400 }, headers: header_with_ua });
                 const response_cid_json = await response_cid.json();
                 const p = id_r[1] ? Number.parseInt(id_r[1]) : 0;
                 const params = `bvid=${id_r[0]}&cid=${response_cid_json['data'][p]["cid"]}&qn=0&fnver=0&fnval=4048&fourk=1&gaia_source=&from_client=BROWSER&voice_balance=1&web_location=1315873&wts=${Date.now() / 1e3}`;
 
                 // 缓存5分钟
-                const response_play_info = await fetch("https://api.bilibili.com/x/player/playurl?" + await BiliBiliUtils.encWbi(params), { next: { revalidate: 300 } });
+                const response_play_info = await fetch("https://api.bilibili.com/x/player/playurl?" + await BiliBiliUtils.encWbi(params), { next: { revalidate: 300 }, headers: header_with_ua });
                 const response_play_info_json = await response_play_info.json();
 
                 audio_url = response_play_info_json['data']['dash']['audio'][0]['baseUrl'];
