bf9c748c05e2219aa272f9d8a10b34280232d1da
WisW <78122384+winsrewu@users.noreply.github.com>
2024-10-01 14:43:53 +0800

    fix: headers modify

--- app/net/proxy/route.ts
+++ app/net/proxy/route.ts
@@ -18,8 +18,19 @@ export async function GET(request: Request) {
         url = url.replace('http://', 'https://');
     }
 
-    let headers = request.headers;
-    headers.set('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36 Edg/127.0.0.0');
+    let headers = new Headers();
+    headers.set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/128.0");
+    headers.set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8");
+    headers.set("Accept-Language", "en-US,en;q=0.5");
+    headers.set("Accept-Encoding", "gzip, deflate, br, zstd");
+    headers.set("DNT", "1");
+    headers.set("Connection", "keep-alive");
+    headers.set("Upgrade-Insecure-Requests", "1");
+    headers.set("Sec-Fetch-Dest", "document");
+    headers.set("Sec-Fetch-Mode", "navigate");
+    headers.set("Sec-Fetch-Site", "none");
+    headers.set("Sec-Fetch-User", "?1");
+    headers.set("Priority", "u=0, i");
 
     try {
         // 注意, 缓存1天
