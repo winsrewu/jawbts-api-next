abc471f54233738686e04dd418c67468b7b210db
WisW <78122384+winsrewu@users.noreply.github.com>
2024-10-05 13:41:22 +0800

    fix: no html now

--- app/not-found.tsx
+++
@@ -1,8 +0,0 @@
-export default function NotFound() {
-    return (
-        <div>
-            <p>Not Found</p>
-            <p>Seems nothing here. Maybe wrong route segments?</p>
-        </div>
-    );
-}
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
-export default function Page() {
-    return (
-        <div>
-            <p>Hello! This works good!</p>
-            <p>But nothing here. Maybe wrong route segments?</p>
-        </div>
-    );
+import { ResponseUtils } from "@/components/ResponseUtils";
+
+export default function GET() {
+    return ResponseUtils.success();
 }
