41b632daea552271ff85e82ecddbf58a3509bd8c
WisW <78122384+winsrewu@users.noreply.github.com>
2024-10-12 23:09:49 +0800

    feat: block all 404 requests

---
+++ middleware.ts
@@ -0,0 +1,13 @@
+import { NextRequest, NextResponse } from "next/server";
+import { ResponseUtils } from "./components/ResponseUtils";
+
+const allowedPathNames = ["/music/get", "music/get/info", "next/proxy", "/favicon.ico", "_next/.*"]
+
+export function middleware(request: NextRequest) {
+    const { pathname } = request.nextUrl;
+    // console.log(pathname + " is being requested")
+    if (!allowedPathNames.some(allowedPath => new RegExp(`^${allowedPath}(/|\\?)?(\\?[^/]+)?$`).test(pathname))) {
+        return ResponseUtils.wrong("pathname. Nothing here.");
+    }
+    return NextResponse.next();
+}
